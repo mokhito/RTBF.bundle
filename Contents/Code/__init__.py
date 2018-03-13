af736f225a7648328d0384860efc67a3eac1c7ec
mokhito <mokhito@users.noreply.github.com>
2018-03-13 16:32:04 +0100

    Fix indentation.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -28,19 +28,19 @@ def MainMenu():
 @route(PREFIX + '/showmenu')
 def ShowsMenu(title):
 
-    oc = ObjectContainer(title2=title)
-    page = HTML.ElementFromURL(SHOWS_URL)
+	oc = ObjectContainer(title2=title)
+	page = HTML.ElementFromURL(SHOWS_URL)
 
-		shows_elements = page.xpath("//article[@class='rtbf-media-item']")
+	shows_elements = page.xpath("//article[@class='rtbf-media-item']")
 
-		for show_elem in shows_elements:
-  		show_title = show_elem.xpath("//a[@class='www-faux-link']//@title")[0]
-			thumb = show_elem.xpath('.//img/@data-srcset')[0].split(',')[0].split(' ')[0]
+	for show_elem in shows_elements:
+		show_title = show_elem.xpath("//a[@class='www-faux-link']//@title")[0]
+		thumb = show_elem.xpath('.//img/@data-srcset')[0].split(',')[0].split(' ')[0]
 
-			oc.add(DirectoryObject(key=Callback(GetShowVideos, id=show_id), 
-				title = show_title,
-				thumb = Resource.ContentsOfURLWithFallback(url=thumb)
-			))
+		oc.add(DirectoryObject(key=Callback(GetShowVideos, id=show_id), 
+			title = show_title,
+			thumb = Resource.ContentsOfURLWithFallback(url=thumb)
+		))
 
 
-    return oc
+	return oc
